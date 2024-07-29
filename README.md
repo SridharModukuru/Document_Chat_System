6dd90beaa89c02b86cb5927be4a0adfed94f10c2
SridharModukuru <sridharmodukuru@gmail.com>
2024-07-29 15:39:17 +0530

    initial commit

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # Document_Chat_System
 
-# API_KEY = 'AIzaSyD4pGO5qSCRqEDP8Vib7iNOuj6wfHQYrcA'
+
--- main.py
+++ main.py
@@ -100,4 +100,5 @@ def user_input(user_question):
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True,host='0.0.0.0',port=5000)
+    
